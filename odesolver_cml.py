5bcee61c7703a99f2c6baf9f27545e5d54ea905e
Kenneth Tochihara <ktt3@illinois.edu>
2019-04-18 21:45:36 -0500

    yeet

--- odesolver_cml.py
+++ odesolver_cml.py
@@ -21,7 +21,6 @@ import matplotlib.pyplot as plt
 methods = {
     'fe' : "Forward-Euler Method",
     'mp' : "Midpoint Method",
-    'h'  : "Heum's Method",
     '2rk': "2nd-Order Runge-Kutta Method",
     '4rk': "4th-Order Runge-Kutta Method",
     '3ab': "3rd-order Adams-Bashforth Method"
@@ -29,9 +28,9 @@ methods = {
 
 # Taking in the values
 f    = StringFunction(input("f(u, t) =  "), independent_variables=('u','t'))
-u0   = float(input("u0 =  "))
-dt   = float(input("dt = "))
-T    = float(input("T = "))
+u0   = float(input("u0 =  ")) #initial condition
+dt   = float(input("dt = "))  #time step
+T    = float(input("T = "))   #Final time of simulation
 type = input("Solve Method: ")
 
 #Calclating the time array, arrT
@@ -47,20 +46,36 @@ arrT.append(T)
 #------------------------------------------------------------------------------#
 
 # Setting functions for the different methods
-def forwardEuler(u0, arrT, f ,T):
-    return 0
+def forwardEuler(u0, arrT, f):
+    u = [u0]
+    for i in range(1, len(arrT)):
+        u.append(u[i-1] + dt * f(u[i-1], arrT[i-1]))
+    return u
 
 def midPoint(u0, arrT, f ,T):
-    return 0
-
-def heum(u0, arrT, f ,T):
-    return 0
+    u = [u0]
+    for i in range(1, len(arrT)):
+        us = u[i-1] + dt * f(u[i-1], arrT[i-1])
+        u.append(u[i-1] + (0.5 * dt * f(u[i-1], arrT[i-1])) + (0.5 * dt * f(us, arrT[i])))
+    return u
 
 def rungeKutta2(u0, arrT, f ,T):
-    return 0
+    u = [u0]
+    for i in range(1, len(arrT)):
+        k1 = dt * f(u[i-1], arrT[i-1])
+        k2 = dt * f(u[i-1] + 0.5 * k1, arrT[i-1] + 0.5 * dt)
+        u.append(u[i-1] + k2)
+    return u
 
 def rungeKutta4(u0, arrT, f ,T):
-    return 0
+    u = [u0]
+    for i in range(1, len(arrT)):
+        k1 = dt * f(u[i-1], arrT[i-1])
+        k2 = dt * f(u[i-1] + 0.5 * k1, arrT[i-1] + 0.5 * dt)
+        k3 = dt * f(u[i-1] + 0.5 * k2, arrT[i-1] + 0.5 * dt)
+        k4 = dt * f(u[i-1] + k3, arrT[i-1] + dt)
+        u.append(u[i-1] + k2)
+    return u
 
 def adamBashforth3(u0, arrT, f ,T):
     return 0
@@ -77,8 +92,6 @@ if   ('fe'  == type):
     arrU = forwardEuler(u0, arrT, f ,T)
 elif ('mp'  == type):
     arrU = midPoint(u0, arrT, f ,T)
-elif ('h'   == type):
-    arrU = heum(u0, arrT, f ,T)
 elif ('2rk' == type):
     arrU = rungeKutta2(u0, arrT, f ,T)
 elif ('4rk' == type):
